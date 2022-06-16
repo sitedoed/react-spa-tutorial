333f84a2667626c5bb7cb7721439f2e1c6588e78
sitedoed <ed@sitedoed.com.br>
2022-06-16 20:52:13 -0300

    Google Fonts Added at index.html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './assets/css/style.css';
+import './assets/css/animate.min.css';
+import MainNav from './components/MainNav';
+import Header from './components/Header';
+import WhyReact from './components/WhyReact';
+import GetStarted from './components/GetStarted';
+import UsefulLinks from './components/UsefulLinks';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import GetStarted from './components/GetStarted';
-import Header from './components/Header';
-import MainNav from './components/MainNav';
 import SocialLinks from './components/SocialLinks';
-import UsefulLinks from './components/UsefulLinks';
-import WhyReact from './components/WhyReact';
+
 
 function App() {
   return (
